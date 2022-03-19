1acd8df7e1ba2cd0efa15ec602439b885d6d3799
Spidersbox <Spider@Spidersbox.com>
2022-03-19 22:27:37 +0000

    edits for boost 1.70+ compatibility

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -560,7 +560,7 @@ public:
     }
     bool connect(const std::string& server, const std::string& port)
     {
-        ip::tcp::resolver resolver(stream.get_io_service());
+        ip::tcp::resolver resolver(GetIOService(stream));
         ip::tcp::resolver::query query(server.c_str(), port.c_str());
         ip::tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
         ip::tcp::resolver::iterator end;
@@ -666,7 +666,7 @@ static void RPCListen(boost::shared_ptr< basic_socket_acceptor<Protocol, SocketA
                    const bool fUseSSL)
 {
     // Accept connection
-    AcceptedConnectionImpl<Protocol>* conn = new AcceptedConnectionImpl<Protocol>(acceptor->get_io_service(), context, fUseSSL);
+    AcceptedConnectionImpl<Protocol>* conn = new AcceptedConnectionImpl<Protocol>(GetIOServiceFromPtr(acceptor), context, fUseSSL);
 
     acceptor->async_accept(
             conn->sslStream.lowest_layer(),
@@ -758,8 +758,13 @@ void ThreadRPCServer2(void* parg)
 
     asio::io_service io_service;
 
-    ssl::context context(io_service, ssl::context::sslv23);
-    if (fUseSSL)
+// BOOST 1.66 require old asio interface and syntax is different too
+#if BOOST_VERSION >= 106600L
+	ssl::context context(ssl::context::sslv23);
+#else
+	ssl::context context(io_service, ssl::context::sslv23);
+#endif
+    	if (fUseSSL)
     {
         context.set_options(ssl::context::no_sslv2);
 
@@ -774,7 +779,13 @@ void ThreadRPCServer2(void* parg)
         else printf("ThreadRPCServer ERROR: missing server private key file %s\n", pathPKFile.string().c_str());
 
         string strCiphers = GetArg("-rpcsslciphers", "TLSv1+HIGH:!SSLv2:!aNULL:!eNULL:!AH:!3DES:@STRENGTH");
+//        SSL_CTX_set_cipher_list(context.impl(), strCiphers.c_str());
+    // BOOST 1.66 require old asio interface and syntax is different too
+#if BOOST_VERSION >= 106600L
+        SSL_CTX_set_cipher_list(context.native_handle(), strCiphers.c_str());
+#else
         SSL_CTX_set_cipher_list(context.impl(), strCiphers.c_str());
+#endif
     }
 
     // Try a dual IPv6/IPv4 socket, falling back to separate IPv4 and IPv6 sockets
@@ -1068,7 +1079,13 @@ Object CallRPC(const string& strMethod, const Array& params)
     // Connect to localhost
     bool fUseSSL = GetBoolArg("-rpcssl");
     asio::io_service io_service;
-    ssl::context context(io_service, ssl::context::sslv23);
+
+// BOOST 1.66 require old asio interface and syntax is different too
+#if BOOST_VERSION >= 106600L
+	ssl::context context(ssl::context::sslv23);
+#else
+	ssl::context context(io_service, ssl::context::sslv23);
+#endif
     context.set_options(ssl::context::no_sslv2);
     asio::ssl::stream<asio::ip::tcp::socket> sslStream(io_service, context);
     SSLIOStreamDevice<asio::ip::tcp> d(sslStream, fUseSSL);
